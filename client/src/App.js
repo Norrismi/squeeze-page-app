4ee80b5edf50480f574208706445da84730bd676
Norrismi <Norrismi0@gmail.com>
2018-11-13 16:45:33 -0500

    adding client

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TextBoxes from './components/Text-Boxes'
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TextBoxes/>
+      </div>
+    );
+  }
+}
+
+export default App;
