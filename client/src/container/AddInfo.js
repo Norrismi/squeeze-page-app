a6671478dd047572aae7b087668819787427c5a2
Norrismi <Norrismi0@gmail.com>
2018-11-28 22:04:32 -0500

    validation complete

--- client/src/container/AddInfo.js
+++ client/src/container/AddInfo.js
@@ -1,42 +1,100 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+let emailRegex = RegExp(/[^@]+@[^.]+\..+/);
+
+// const formValid = formErrors => {
+//   let valid = true;
+
+//   Object.values(formErrors).forEach(val => val && (valid = false));
+//   return valid;
+// };
+
 class TextBoxes extends Component {
-  state = {
-    formData: {
+  constructor(props) {
+    super(props);
+
+    this.state = {
       firstName: "",
       lastName: "",
       email: "",
-      phone: ""
-    }
-  };
+      phone: "",
 
-  handleInput = (event, name) => {
-    const newFormData = {
-      ...this.state.formData
+      formErrors: {
+        firstName: "",
+        lastName: "",
+        email: "",
+        phone: "",
+        form: []
+      }
     };
-    newFormData[name] = event.target.value;
+  }
 
-    this.setState({
-      formData: newFormData
-    });
-  };
-
-  submitForm = event => {
+  handleSubmit = event => {
     event.preventDefault();
 
-    const data = this.state.formData;
+    const data = this.state;
+    const all = {...this.state}
+    console.log(all)
+    
+    this.state.firstName && this.state.phone
+      ? axios.post("/", data).then(res => {
+          this.props.history.push("/thanks");
+        })
+      : data.formErrors.form.push("Complete the form");
 
-    axios.post("/", data).then(res => {
-      this.props.history.push("/thanks");
-    });
+    // if (formValid(this.state.formErrors)) {
+    //   let data = this.state;
+
+    //   console.log(`
+    //   FirstName: ${data.firstName}
+    //   Last Name: ${data.lastName}
+    //   Email: ${data.email}
+    //   Phone: ${data.phone}
+    //   `);
+    // } else {
+    //   console.error("Form Invalid - Display error message");
+    // }
+  };
+
+  handleChange = event => {
+    event.preventDefault();
+    const { name, value } = event.target; //name in JSX, value = what's typed in
+    let formErrors = this.state.formErrors;
+
+    switch (name) {
+      case "firstName":
+        formErrors.firstName =
+          value.length < 3 ? "minimum 3 characters required" : null;
+        break;
+      case "lastName":
+        formErrors.lastName =
+          value.length < 2 ? "minimum 2 characters required" : null;
+        break;
+      case "email":
+        formErrors.email = emailRegex.test(value)
+          ? null
+          : "Please enter a valid email address";
+        break;
+
+      case "phone":
+        formErrors.phone =
+          value.length < 7 ? "minimum 7 characters required" : null;
+        break;
+    
+
+      default:
+        break;
+    }
+    this.setState({ formErrors, [name]: value });
   };
 
   render() {
-    console.log(this.state.formData);
+    console.log(this.state)
+    let { formErrors } = this.state;
     return (
       <div className="container">
-        <form onSubmit={this.submitForm}>
+        <form onSubmit={this.handleSubmit}>
           <div className="row">
             <div className="col s6">
               <div className="row" />
@@ -45,10 +103,15 @@ class TextBoxes extends Component {
               <input
                 id="icon_prefix"
                 type="text"
+                name="firstName"
+                placeholder="First Name"
                 value={this.state.firstName}
-                onChange={event => this.handleInput(event, "firstName")}
+                onChange={this.handleChange}
               />
-              <label>First Name</label>
+
+              {formErrors.firstName ? (
+                <span>{formErrors.firstName}</span>
+              ) : null}
             </div>
 
             <div className="row">
@@ -59,10 +122,14 @@ class TextBoxes extends Component {
                 <input
                   id="icon_prefix"
                   type="text"
+                  name="lastName"
+                  placeholder="Last Name"
                   value={this.state.lastName}
-                  onChange={event => this.handleInput(event, "lastName")}
+                  onChange={this.handleChange}
                 />
-                <label>Last Name</label>
+                {formErrors.lastName ? (
+                  <span>{formErrors.lastName}</span>
+                ) : null}
               </div>
             </div>
           </div>
@@ -75,10 +142,12 @@ class TextBoxes extends Component {
               <input
                 id="email"
                 type="email"
+                name="email"
+                placeholder="Email"
                 value={this.state.email}
-                onChange={event => this.handleInput(event, "email")}
+                onChange={this.handleChange}
               />
-              <label>Email</label>
+              {formErrors.email ? <span>{formErrors.email}</span> : null}
             </div>
           </div>
           <div className="row">
@@ -89,18 +158,21 @@ class TextBoxes extends Component {
               <input
                 id="icon_telephone"
                 type="tel"
+                name="phone"
+                placeholder="Cell Phone"
                 value={this.state.phone}
-                onChange={event => this.handleInput(event, "phone")}
+                onChange={this.handleChange}
               />
-
-              <label>Phone Number</label>
+              {formErrors.phone ? <span>{formErrors.phone}</span> : null}
             </div>
           </div>
 
           <button className="form btn" type="submit">
             Submit
           </button>
+
         </form>
+          {formErrors.form ? <span>{formErrors.form[0]}</span>:null}
       </div>
     );
   }
